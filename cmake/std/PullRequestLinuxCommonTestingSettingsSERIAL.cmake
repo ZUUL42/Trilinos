b82bee7fb89da39b903a86d6f1f977a22314826d
Henry Swantner <HRSwant@sandia.gov>
2019-10-16 14:45:25 -0600

    TEST: Adjust 7.2.0 build files to take on some properties of the 4.9.3 SERIAL build files.

--- cmake/std/PullRequestLinuxCommonTestingSettingsSERIAL.cmake
+++ cmake/std/PullRequestLinuxCommonTestingSettingsSERIAL.cmake
@@ -11,26 +11,23 @@
 # Use the below option only when submitting to the dashboard
 #set (CTEST_USE_LAUNCHERS ON CACHE BOOL "Set by default for PR testing")
 
-set (Trilinos_TEST_CATEGORIES BASIC CACHE STRING "Set by default for PR testing")
-
 set (Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES         ON CACHE BOOL "Set by default for PR testing")
 set (Trilinos_ALLOW_NO_PACKAGES                    ON CACHE BOOL "Set by default for PR testing")
 set (Trilinos_DISABLE_ENABLED_FORWARD_DEP_PACKAGES ON CACHE BOOL "Set by default for PR testing")
 set (Trilinos_ENABLE_SECONDARY_TESTED_CODE         ON CACHE BOOL "Set by default for PR testing")
 set (Trilinos_IGNORE_MISSING_EXTRA_REPOSITORIES    ON CACHE BOOL "Set by default for PR testing")
 set (Trilinos_ENABLE_TESTS                         ON CACHE BOOL "Set by default for PR testing")
+set (Trilinos_TEST_CATEGORIES BASIC CACHE STRING "Set by default for PR testing")
 set (Trilinos_ENABLE_CONFIGURE_TIMING              ON CACHE BOOL "Set by default for PR testing")
 set (Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES      ON CACHE BOOL "Set by default for PR testing")
 set (Trilinos_CTEST_USE_NEW_AAO_FEATURES           ON CACHE BOOL "Set by default for PR testing")
 
 # Options from cmake/std/MpiReleaseDebugSharedPtSettings.cmake
 
-set (CMAKE_BUILD_TYPE RELEASE CACHE STRING "Set by default for PR testing")
-
-set (BUILD_SHARED_LIBS ON CACHE BOOL "Set by default for PR testing")
-
 set (TPL_ENABLE_MPI                         OFF CACHE BOOL "Set by default for PR testing")
+set (CMAKE_BUILD_TYPE RELEASE CACHE STRING "Set by default for PR testing")
 set (Trilinos_ENABLE_DEBUG                  ON  CACHE BOOL "Set by default for PR testing")
+set (BUILD_SHARED_LIBS ON CACHE BOOL "Set by default for PR testing")
 set (Trilinos_ENABLE_DEBUG_SYMBOLS          ON  CACHE BOOL "Set by default for PR testing")
 set (Trilinos_ENABLE_EXPLICIT_INSTANTIATION ON  CACHE BOOL "Set by default for PR testing")
 set (Trilinos_ENABLE_SECONDARY_TESTED_CODE  OFF CACHE BOOL "Set by default for PR testing")
@@ -57,16 +54,15 @@ set (Trilinos_TRACE_ADD_TEST ON  CACHE BOOL "Set by default for PR testing")
 
 set (TPL_ENABLE_Scotch OFF CACHE BOOL "Set by default for PR testing")
 
+# Disable long-failing Piro test until it can be fixed (#826)
+set (Piro_EpetraSolver_MPI_4_DISABLE ON CACHE BOOL "Set by default for PR testing")
+
 # Options from SEMSDevEnv.cmake
 SET(CMAKE_C_COMPILER "$ENV{CC}" CACHE FILEPATH "Set by default for PR testing")
 
 SET(CMAKE_CXX_COMPILER "$ENV{CXX}" CACHE FILEPATH "Set by default for PR testing")
 
 SET(CMAKE_Fortran_COMPILER "$ENV{F90}" CACHE FILEPATH "Set by default for PR testing")
-
-# SET(MPI_BASE_DIR "$ENV{SEMS_OPENMPI_ROOT}" CACHE PATH "Set by default for PR testing")
-
-#SET(Trilinos_EXTRA_LINK_FLAGS "-lgomp -lgfortran -lldl -ldl" CACHE STRING "Set by default for PR testing")
 
 SET(Trilinos_ENABLE_PyTrilinos OFF CACHE BOOL "Set by default for PR testing")
 
@@ -82,9 +78,6 @@ SET(BoostLib_LIBRARY_DIRS "$ENV{SEMS_BOOST_LIBRARY_PATH}" CACHE PATH "Set by def
 SET(METIS_INCLUDE_DIRS "$ENV{SEMS_METIS_INCLUDE_PATH}" CACHE PATH "Set by default for PR testing")
 SET(METIS_LIBRARY_DIRS "$ENV{SEMS_METIS_LIBRARY_PATH}" CACHE PATH "Set by default for PR testing")
 
-# SET(ParMETIS_INCLUDE_DIRS "$ENV{SEMS_PARMETIS_INCLUDE_PATH}" CACHE PATH "Set by default for PR testing")
-# SET(ParMETIS_LIBRARY_DIRS "$ENV{SEMS_PARMETIS_LIBRARY_PATH}" CACHE PATH "Set by default for PR testing")
-
 SET(Zlib_INCLUDE_DIRS "$ENV{SEMS_ZLIB_INCLUDE_PATH}" CACHE PATH "Set by default for PR testing")
 SET(Zlib_LIBRARY_DIRS "$ENV{SEMS_ZLIB_LIBRARY_PATH}" CACHE PATH "Set by default for PR testing")
 
@@ -96,9 +89,3 @@ SET(Netcdf_LIBRARY_DIRS "$ENV{SEMS_NETCDF_LIBRARY_PATH}" CACHE PATH "Set by defa
 
 SET(SuperLU_INCLUDE_DIRS "$ENV{SEMS_SUPERLU_INCLUDE_PATH}" CACHE PATH "Set by default for PR testing")
 SET(SuperLU_LIBRARY_DIRS "$ENV{SEMS_SUPERLU_LIBRARY_PATH}" CACHE PATH "Set by default for PR testing")
-
-# set (TPL_Scotch_INCLUDE_DIRS "$ENV{SEMS_SCOTCH_INCLUDE_PATH}" CACHE PATH "Set by default for PR testing")
-# set (Scotch_LIBRARY_DIRS "$ENV{SEMS_SCOTCH_LIBRARY_PATH}" CACHE PATH "Set by default for PR testing")
-
-
-
--- cmake/std/sems/PullRequestGCC7.2.0TestingEnv.sh
+++ cmake/std/sems/PullRequestGCC7.2.0TestingEnv.sh
@@ -1,21 +1,21 @@
 # This script can be used to load the appropriate environment for the
-# GCC 7.2.0 Pull Request testing build on a Linux machine that has access to
+# GCC 7.2.0 SERIAL Pull Request testing build on a Linux machine that has access to
 # the SEMS NFS mount.
 
-# usage: $ source PullRequestGCC7.2.0TestingEnv.sh
+# usage: $ source PullRequestGCC7.2.0SERIALTestingEnv.sh
 
 source /projects/sems/modulefiles/utils/sems-modules-init.sh
 
 module load sems-gcc/7.2.0
-module load sems-openmpi/1.10.1
 module load sems-python/2.7.9
 module load sems-git/2.10.1
 module load sems-boost/1.63.0/base
 module load sems-zlib/1.2.8/base
-module load sems-hdf5/1.8.12/parallel
-module load sems-netcdf/4.4.1/exo_parallel
-module load sems-parmetis/4.0.3/parallel
-module load sems-scotch/6.0.3/nopthread_64bit_parallel
+module load sems-hdf5/1.8.12/base
+module load sems-netcdf/4.4.1/exo
+module load sems-openmpi/1.10.1
+module load sems-metis/5.1.0/base
+# module load sems-scotch/6.0.3/nopthread_64bit_parallel
 module load sems-superlu/4.3/base
 
 # Load the SEMS CMake Module
@@ -31,8 +31,8 @@ module load sems-cmake/3.10.3
 # available in main-line Ninja) to significantly speed up
 # compile and link times.
 module load atdm-env
+module load atdm-cmake/3.11.1
 module load atdm-ninja_fortran/1.7.2
 
 # add the OpenMP environment variable we need
 export OMP_NUM_THREADS=2
-
