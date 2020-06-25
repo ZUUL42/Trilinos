fc18431f21b76f4fee46b53765e4b39bda9f9eac
Henry Swantner <HRSwant@sandia.gov>
2020-06-24 22:37:45 -0600

    Update sh file for consistency.

--- cmake/std/sems/PullRequestCuda10.1.105TestingEnv.sh
+++ cmake/std/sems/PullRequestCuda10.1.105TestingEnv.sh
@@ -1,13 +1,15 @@
 # This script can be used to load the appropriate environment for the
 # PR build on ride using CUDA.
 
-# usage: $ source PullRequestCUDA9.2TestingEnv.sh
+# usage: $ source PullRequestCUDA10.1.105TestingEnv.sh
 
 module load git/2.10.1
 module load devpack/20190404/openmpi/4.0.1/gcc/7.2.0/cuda/10.1.105
 module swap openblas/0.3.4/gcc/7.4.0 netlib/3.8.0/gcc/7.2.0
 #export OMPI_CXX=`which g++`
-export OMPI_CXX=$WORKSPACE/Trilinos/packages/kokkos/bin/nvcc_wrapper
+current_dir=`dirname $BASH_SOURCE`
+Trilinos_dir=`realpath $current_dir/../../..`
+export OMPI_CXX=${Trilinos_dir}/packages/kokkos/bin/nvcc_wrapper
 export OMPI_CC=`which gcc`
 export OMPI_FC=`which gfortran`
 export CUDA_LAUNCH_BLOCKING=1
